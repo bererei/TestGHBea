90d72d39758eff285ccc9346b80cfc317bae991f
BeatrizGD <breyesgd@gmail.com>
2022-10-03 19:45:15 +0000

    Borrar preprocessing old

--- preprocessing.py
+++
@@ -1,9 +0,0 @@
-"""
-Este módulo contiene helpers (funciones auxiliares) para el preprocesamiento de datos.
-"""
-
-import numpy as np  # Ahora quedará declarada en .toml
-
-def get_numerical_features(df):
-    """ Función que devuelve todas las variables cuantitativas de un dataframe"""
-    return df.select_dytypes(include=[np.number]).columns
