d77f15a62255e57418db6bba28c6d0d786db4c0c
Beatriz <breyesgd@gmail.com>
2022-10-19 12:08:52 +0100

    Starting with Linux in WSL

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -1,5 +1,5 @@
 """
-Este módulo contiene helpers (funciones auxiliares) para el preprocesamiento de datos.
+Este módulo contiene helpers (funciones auxiliares o herramientas) para el preprocesamiento de datos.
 """
 
 import numpy as np  # Ahora quedará declarada en .toml
@@ -23,6 +23,7 @@ def get_numerical_features(df) -> list[str]:
     >>> get_numerical_features(df)
     ['a']
     """
+
     return list(
         df.select_dtypes(include=[np.number]).columns
     )  # Lo casteo a lista para que aunque esté vacío me de una lista.
